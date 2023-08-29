ed5ccc9feb56c7997d1ad392e4aee82eb6fbf1cb
Olga Oblat <oa.oblat@gmail.com>
2023-08-29 16:43:00 +0300

    changed src

---
+++ src/main/java/ru/netology/auth/RegistrationDto.java
@@ -0,0 +1,13 @@
+package ru.netology.auth;
+
+public class RegistrationDto {
+    private String login;
+    private String password;
+    private String status;
+
+    public RegistrationDto(String login, String password, String status) {
+        this.login = login;
+        this.password = password;
+        this.status = status;
+    }
+}
