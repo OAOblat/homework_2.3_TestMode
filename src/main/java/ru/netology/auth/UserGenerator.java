e0c639cc511eb24cc7b1ab07c4ce91bb1ef9ba1d
Olga Oblat <oa.oblat@gmail.com>
2023-08-30 19:51:35 +0300

    changed src

--- src/main/java/ru/netology/auth/UserGenerator.java
+++ src/main/java/ru/netology/auth/UserGenerator.java
@@ -5,7 +5,7 @@ import com.github.javafaker.Faker;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
-import  io.restassured.response.Response;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 import static io.restassured.RestAssured.given;
@@ -20,15 +20,14 @@ public class UserGenerator {
             .setContentType(ContentType.JSON)
             .log(LogDetail.ALL)
             .build();
-    private static final Faker faker = new Faker();
 
         public static String generateRandomUserActive() {
             Faker faker = new Faker();
-            String username = faker.name().username();
+            String login = faker.name().username();
             String password = faker.internet().password();
 
             // Отправляем POST-запрос для сохранения пользователя и получаем его в ответе
-            RegistrationDto registrationDto = new RegistrationDto(username, password, "active");
+            RegistrationDto registrationDto = new RegistrationDto(login, password, "active");
             Response response = given()
                     .spec(requestSpec)
                     .body(registrationDto)
@@ -39,32 +38,14 @@ public class UserGenerator {
                     .log().all() // строчка для логирования ответа
                     .extract()
                     .response();
-
-
             // Извлекаем значения пользователя из ответа
-            String savedUsername = response.jsonPath().getString("username");
+            String savedLogin = response.jsonPath().getString("login");
             String savedPassword = response.jsonPath().getString("password");
 
             // Возвращаем значения в виде строки
-            return savedUsername + ":" + savedPassword;
+            return savedLogin + ":" + savedPassword;
 
         }
 
-    public static RegistrationDto generateRandomUserBlocked() {
-        String username = faker.name().username();
-        String password = faker.internet().password();
-        RegistrationDto registrationDto = new RegistrationDto(username, password, "blocked");
-        RegistrationDto userBlocked = given()
-                .spec(requestSpec)
-                .body(registrationDto)
-                .when()
-                .post("/api/system/users")
-                .then()
-                .statusCode(200)
-                .extract()
-                .as(RegistrationDto.class);
-
-        return userBlocked;
-    }
 }
 
--- src/test/java/ru/netology/auth/AuthTest.java
+++ src/test/java/ru/netology/auth/AuthTest.java
@@ -23,18 +23,19 @@ class AuthTest {
             // Генерируем случайного пользователя и сохраняем значения в переменной userData
             String userData = generateRandomUserActive();
 
-            // Разделяем значения имени пользователя и пароля
+        // Разделяем значения имени пользователя и пароля
             String[] userValues = userData.split(":");
-            String savedUsername = userValues[0];
+            String savedLogin = userValues[0];
             String savedPassword = userValues[1];
 
             SelenideElement form = $(".form_theme_alfa-on-white");
-        form.$("[data-test-id=login] input").sendKeys(savedUsername);
+        form.$("[data-test-id=login] input").sendKeys(savedLogin);
         form.$("[data-test-id=password] input").sendKeys(savedPassword);
         form.$("[data-test-id=action-login]").click();
         $(".heading_theme_alfa-on-white")
                 .shouldHave(Condition.text("Личный кабинет"), Duration.ofSeconds(15))
                 .shouldBe(Condition.visible);
+
     }
 
 
